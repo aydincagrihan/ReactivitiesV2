a21baae7777236bb6b339dfe6b2a83151dec1b5d
Çağrıhan Aydın <cagrihanaydin@gmail.com>
2023-04-14 15:53:53 +0300

    Ch6,axios and agent confg.

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,10 +1,10 @@
 import React, { Fragment, useEffect, useState } from 'react';
-import axios from 'axios';
 import { Container } from 'semantic-ui-react';
 import {Activity} from '../models/activity'
 import NavBar from './NavBar';
 import ActivityDasboard from '../../features/activities/dashboard/ActivityDashboard';
 import {v4 as uuid} from 'uuid';
+import agent from '../api/agent';
 
 function App() {
 const [activities,setActivities]=useState<Activity[]>([]);
@@ -12,10 +12,12 @@ const[selectedActivity,setSelectedActivity] = useState<Activity|undefined>(undef
 const[editMode,setEditMode] = useState(false);
 
 useEffect(()=>{
-axios.get<Activity[]>('http://localhost:5000/api/activities').then(response=>{
-  console.log(response);
-  setActivities(response.data);
-})
+// axios.get<Activity[]>('http://localhost:5000/api/activities').then(response=>{
+//   console.log(response);
+//   setActivities(response.data);
+// })
+agent.Activities.list().then(response=>{setActivities(response)})
+
 },[])
 
 function handleSelectActivity(id:string){
