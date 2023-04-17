93be9fc3f4833cbe904b5da322bde2d09eed94ed
Çağrıhan Aydın <cagrihanaydin@gmail.com>
2023-04-17 17:42:49 +0300

    Less 73. Mobx refactoring legacy code

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,12 +1,36 @@
-import { makeObservable, observable } from "mobx";
+import {
+  makeAutoObservable,
+} from "mobx";
+import { Activity } from "../models/activity";
+import agent from "../api/agent";
 
+export default class ActivityStore {
+  activities: Activity[] = [];
+  selectedActivity: Activity | null = null;
+  editMode: boolean = false;
+  loading: boolean = false;
+  loadingInitial: boolean = false;
 
-export default class ActivityStore{
-    title= 'Hello MobX';
-    constructor(){
-        makeObservable(this,{
-            title:observable
-        })
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  loadActivities = async () => {
+    this.setLoadingInitial(true);
+    try {
+      const activities = await agent.Activities.list();
+      activities.forEach((activity) => {
+        //response gelip datalar basılmadan önce tarih formatında düzenleme yaptım
+        activity.date = activity.date.split("T")[0];
+        this.activities.push(activity);
+      });
+      this.setLoadingInitial(false);
+    } catch (error) {
+      console.log(error);
+      this.setLoadingInitial(false);
     }
-
+  };
+  setLoadingInitial = (state: boolean) => {
+    this.loadingInitial = state;
+  };
 }
