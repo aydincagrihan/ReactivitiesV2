0eb0c5cd424ed5b0974d9989c8ded6625ab0db45
Çağrıhan Aydın <cagrihanaydin@gmail.com>
2023-04-17 16:58:45 +0300

    Ch7. Less 69. MobX Start!

---
+++ client-app/src/app/stores/activityStore.ts
@@ -0,0 +1,12 @@
+import { makeObservable, observable } from "mobx";
+
+
+export default class ActivityStore{
+    title= 'Hello MobX';
+    constructor(){
+        makeObservable(this,{
+            title:observable
+        })
+    }
+
+}
