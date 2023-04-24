b8fd12eb4afd0e4b8baff9b376b256282c5b6cc2
Çağrıhan Aydın <cagrihanaydin@gmail.com>
2023-04-24 15:40:57 +0300

    Ch8. Less 81 Navlink kullanarak navbara link eklendi

--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -1,27 +1,22 @@
-import React from 'react';
 import { Button, Container, Menu } from 'semantic-ui-react';
-import { useStore } from '../stores/store';
-
-
+import { NavLink } from 'react-router-dom';
 
 export default function NavBar() {
-const {activityStore}=useStore();
-
     return (
 
         <Menu
             inverted fixed='top'>
             <Container>
-                <Menu.Item header>
+                <Menu.Item as={NavLink} to='/' header>
                     <img src="/assets/logo.png" alt="logo" style={{marginRight:10}} />
                     Han
                 </Menu.Item>
-                <Menu.Item name='Activities' />
+                <Menu.Item as={NavLink} to='/activities' name='Activities' />
                 <Menu.Item >
                     <Button
                         content="Create Activitiy"
                         positive
-                        onClick={()=>activityStore.openForm()}
+                        as={NavLink} to='/createActivity'
                     ></Button>
 
                 </Menu.Item>
