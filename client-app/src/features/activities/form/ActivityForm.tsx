6a865f1e4e611f1eccfb32927b36a13c245ded48
Çağrıhan Aydın <cagrihanaydin@gmail.com>
2023-05-18 11:25:13 +0300

    Less 87. react routerda useNavigate() kullanımı bununla history push yerine react router kullanılıyorsa navigate kullanmak daha mantıklı

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,19 +1,21 @@
-import  { ChangeEvent, useEffect, useState } from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
 import { Button, Form, Segment } from 'semantic-ui-react';
 import { Activity } from '../../../app/models/activity';
 import { useStore } from '../../../app/stores/store';
 import { observer } from 'mobx-react-lite';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import LoadingComponent from '../../../app/layout/LoadingComponent';
+import { v4 as uuid } from 'uuid';
 
 
 
-export default observer( function ActivityForm(){
-    const {activityStore}=useStore();
-    const{selectedActivity,createActivity,updateActivity,loading,loadActivity,loadingInitial}=activityStore
+export default observer(function ActivityForm() {
+    const { activityStore } = useStore();
+    const { selectedActivity, createActivity, updateActivity, loading, loadActivity, loadingInitial } = activityStore
 
-    const {id}=useParams();
-    const [activity,setActivity]=useState<Activity>({
+    const { id } = useParams();
+    const navigate = useNavigate();
+    const [activity, setActivity] = useState<Activity>({
         id: '',
         title: '',
         date: '',
@@ -23,29 +25,36 @@ export default observer( function ActivityForm(){
         venue: '',
     });
 
-    useEffect(()=>{
+    useEffect(() => {
         //activitiy "!" sonuna koymamın nedeni bu ifadenin null or undefined 
         //olmayacağı anlamına gelir yoksa tip güvenliğinden hata return ediyordu
         //TypeScript functionality kapatmak için ! kullanılır
-        if(id)loadActivity(id).then(activity=>setActivity(activity!))
-    },[id,loadActivity])
+        if (id) loadActivity(id).then(activity => setActivity(activity!))
+    }, [id, loadActivity])
 
 
-// const [activity,setActivity]=useState(initialState);
+    // const [activity,setActivity]=useState(initialState);
 
-function handleSubmit(){
-    activity.id ? updateActivity(activity):createActivity(activity);
-}
+    function handleSubmit() {
+        if (!activity.id) {
+            activity.id = uuid();
+            createActivity(activity).then(() => navigate(`/activities/${activity.id}`))
+        }
+         else
+         {
+            updateActivity(activity).then(() => navigate(`/activities/${activity.id}`))
 
-function handleInputChange(event:ChangeEvent<HTMLInputElement|HTMLTextAreaElement>)
-{
- const {name,value}=event.target;
- setActivity({...activity,[name]:value});
-}
+        }
+    }
 
-if(loadingInitial) return <LoadingComponent content='Aktivite Yükleniyor...'/>
+    function handleInputChange(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
+        const { name, value } = event.target;
+        setActivity({ ...activity, [name]: value });
+    }
 
-    return(
+    if (loadingInitial) return <LoadingComponent content='Aktivite Yükleniyor...' />
+
+    return (
         <Segment clearing>
             <Form onSubmit={handleSubmit} autoComplete='off'>
                 <Form.Input placeholder="Title" value={activity?.title} name="title" onChange={handleInputChange} />
@@ -54,8 +63,8 @@ if(loadingInitial) return <LoadingComponent content='Aktivite Yükleniyor...'/>
                 <Form.Input type='date' placeholder="Date" value={activity?.date} name="date" onChange={handleInputChange} />
                 <Form.Input placeholder="City" value={activity?.city} name="city" onChange={handleInputChange} />
                 <Form.Input placeholder="Venue" value={activity?.venue} name="venue" onChange={handleInputChange} />
-                <Button floated='right' loading={loading} positive type="submit"content="Submit"/>
-                <Button   floated='right'  type="button"content="Cancel"/>
+                <Button floated='right' loading={loading} positive type="submit" content="Submit" />
+                <Button floated='right' type="button" content="Cancel" />
             </Form>
         </Segment>
     )
