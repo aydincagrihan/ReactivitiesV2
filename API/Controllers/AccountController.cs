fb4c043302ceaceddc40ba35c69a97c4afed6606
Çağrıhan Aydın <cagrihanaydin@gmail.com>
2023-11-13 16:32:22 +0300

    Endofthe section 13 login ve register çalışmaları tamamlandı fe den yapılabilmektedir

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -49,12 +49,17 @@ namespace API.Controllers
         {
             if (await _userManager.Users.AnyAsync(x => x.UserName == registerDto.UserName))
             {
-                return BadRequest("Username is already taken");
+
+                ModelState.AddModelError("username", "UserName is already registered");
+
+                return ValidationProblem();
             }
 
             if (await _userManager.Users.AnyAsync(x => x.Email == registerDto.Email))
             {
-                return BadRequest("Email is already taken");
+                ModelState.AddModelError("email", "Email is already registered");
+                return ValidationProblem();
+
             }
             var user = new AppUser
             {
--- client-app/src/features/users/RegisterForm.tsx
+++ client-app/src/features/users/RegisterForm.tsx
@@ -1,10 +1,10 @@
 import { ErrorMessage, Form, Formik } from "formik";
-import { values } from "mobx";
 import MyTextInput from "../../app/common/form/MyTextInput";
-import { Button, Header, Label } from "semantic-ui-react";
+import { Button, Header } from "semantic-ui-react";
 import { observer } from "mobx-react-lite";
 import { useStore } from "../../app/stores/store";
 import * as Yup from 'yup'
+import ValidationError from "../errors/ValidationError";
 
 export default observer(function RegisterForm() {
     const { userStore } = useStore();
@@ -12,26 +12,27 @@ export default observer(function RegisterForm() {
     return (
 
         <Formik
-            initialValues={{displayName:'',username:'', email: '', password: '', error: null }}
-            onSubmit={(values, { setErrors }) => userStore.login(values).catch(error => setErrors({ error: 'Invalid email or password' }))}
-        validationSchema={Yup.object({
-            displayName:Yup.string().required(),
-            username:Yup.string().required(),
-            email:Yup.string().required(),
-            password:Yup.string().required(),
-        })}
+            initialValues={{ displayName: '', username: '', email: '', password: '', error: null }}
+            onSubmit={(values, { setErrors }) => userStore.register(values).catch(error =>
+                setErrors({error}))}
+
+            validationSchema={Yup.object({
+                displayName: Yup.string().required(),
+                username: Yup.string().required(),
+                email: Yup.string().required(),
+                password: Yup.string().required(),
+            })}
         >
-            {({ handleSubmit, isSubmitting, errors,isValid,dirty }) => (
-                <Form className="ui form" onSubmit={handleSubmit} autoComplete="off">
+            {({ handleSubmit, isSubmitting, errors, isValid, dirty }) => (
+                <Form className="ui form error" onSubmit={handleSubmit} autoComplete="off">
                     <Header as='h2' content='Sign up to Reactivities' color="teal" textAlign="center" />
                     <MyTextInput placeholder="DisplayName" name="displayName" />
                     <MyTextInput placeholder="UserName" name="username" />
                     <MyTextInput placeholder="Email" name="email" />
                     <MyTextInput placeholder="Password" name="password" type="password" />
                     <ErrorMessage
-                        name="error" render={() => <Label style={{ marginBottom: 10 }} basic color="red" content={errors.error} />}
-                    />
-                    <Button disabled={!isValid||!dirty||isSubmitting} loading={isSubmitting} positive content='Register' type="submit" fluid />
+                        name="error" render={() => <ValidationError errors={errors.error as unknown as string[]} />}/>
+                    <Button disabled={!isValid || !dirty || isSubmitting} loading={isSubmitting} positive content='Register' type="submit" fluid />
                 </Form>
             )}
         </Formik>
