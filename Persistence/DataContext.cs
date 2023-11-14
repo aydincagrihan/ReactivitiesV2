c66d53e6851c2e493f6b0c1481521e77feeadaf8
Çağrıhan Aydın <cagrihanaydin@gmail.com>
2023-11-14 15:05:50 +0300

    CH14,L157 EF kullanarak ActivityAttendee tablosu oluşturuldu.migration alındı

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -9,7 +9,25 @@ namespace Persistence
         public DataContext(DbContextOptions options) : base(options)
         {
         }
+        //dotnet ef migrations add ActivityAttendee -p Persistence -s API migration komutu
         public  DbSet<Activity> Activities { get; set; }
+        public DbSet<ActivityAttendee> ActivityAttendees { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<ActivityAttendee>(x=>x.HasKey(aa=>new {aa.AppUserId,aa.ActivityId}));
+
+            builder.Entity<ActivityAttendee>()
+            .HasOne(u=>u.AppUser)
+            .WithMany(a=>a.Activities)
+            .HasForeignKey(aa=>aa.AppUserId);
+
+              builder.Entity<ActivityAttendee>()
+            .HasOne(u=>u.Activity)
+            .WithMany(a=>a.Attendees)
+            .HasForeignKey(aa=>aa.ActivityId);
+
+        }
     }
 }
