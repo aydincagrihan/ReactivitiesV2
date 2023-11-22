0b985d06654ecdd8c01ed48e356f764157944695
Çağrıhan Aydın <cagrihanaydin@gmail.com>
2023-11-22 09:54:19 +0300

    CH17,L199 Profil photos component son düzenlemeleri,Ayrıca PhotoUploadWidget oluşturulmaya başlandı bir sonraki adım drag&drop özelliği olacak

--- client-app/src/features/profiles/ProfileContent.tsx
+++ client-app/src/features/profiles/ProfileContent.tsx
@@ -1,10 +1,15 @@
 import { Tab } from "semantic-ui-react"
+import ProfilePhotos from "./ProfilePhotos"
+import { Profile } from "../../app/models/profile"
 
+interface Props{
+    profile:Profile
+}
 
-export default function ProfieContent() {
+export default function ProfieContent({profile}:Props) {
     const panes = [
         { menuItem: 'About', render: () => <Tab.Pane>About Content</Tab.Pane> },
-        { menuItem: 'Photos', render: () => <Tab.Pane>Photos Content</Tab.Pane> },
+        { menuItem: 'Photos', render: () => <ProfilePhotos profile={profile} />},
         { menuItem: 'Events', render: () => <Tab.Pane>Events Content</Tab.Pane> },
         { menuItem: 'Followers', render: () => <Tab.Pane>Followers Content</Tab.Pane> },
         { menuItem: 'Following', render: () => <Tab.Pane>Followingout Content</Tab.Pane> },
